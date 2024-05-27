308fa50bc91ee4061b255b9597d2c7fe31a16f46
Bert Bullough <bert@bertbullough.com>
2024-05-27 14:29:01 -0600

    adds frame rate

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,13 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-    
+        self.clock = pygame.time.Clock()
+
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
@@ -21,8 +23,8 @@ class AlienInvasion:
             pygame.display.flip()
             self.clock.tick(60)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
-    
